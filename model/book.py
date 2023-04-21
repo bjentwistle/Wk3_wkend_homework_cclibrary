dcbf529f724c2ef211183fd0c6995967a925f750
Becky <bjentwistle@gmail.com>
2023-04-21 15:53:57 +0100

    initial commit for sanity sake

---
+++ model/book.py
@@ -0,0 +1,9 @@
+class Book():
+
+    def __init__(self, title, author, genre, loaned, book_location, synopsis):
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.loaned = loaned
+        self.book_location = book_location
+        self.description = synopsis
---
+++ model/books.py
@@ -0,0 +1,5 @@
+from book import Book
+
+#book instance = Book(title, author, genre, loaned, book_location, synopsis)
+book1 = Book("Ender's Game", "Orson Scott-Card", "Sci-Fi", True, "shelf O", "A young boy, Ender, is trained to become the ultimate leader against a deadly enemy")
+book2 = Book()

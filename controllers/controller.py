324c0d0bde860778115de29b99ae2488c37b5aa4
Becky <bjentwistle@gmail.com>
2023-04-21 16:11:11 +0100

    saving progress

---
+++ controllers/controller.py
@@ -0,0 +1,11 @@
+from flask import render_template, redirect, request
+from app import app
+from models.books import books
+from model.book import *
+
+
+
+
+@app.route('/books') #default method is GET
+def index():
+    return render_template('index.html', title = "Home Page", books = books)
---
+++ models/books.py
@@ -0,0 +1,22 @@
+from book import Book
+
+#book instance = Book(title, author, genre, loaned, book_location, synopsis)
+book1 = Book("Ender's Game", "Orson Scott-Card", "Sci-Fi", True, "shelf O", "A young boy, Ender, is trained to become the ultimate leader against a deadly enemy")
+book2 = Book("Persuasion", "Jane Austen", "Period Drama", False, "shelf A", "A lesson in not listening to your elders when love and your happiness is involved")
+
+
+books = [book1, book2]
+
+#Class methods needed:
+
+#def list_all_books():
+
+#def show_one_book(book_title???): 
+
+# def add_book_to_library(book):
+
+#def remove_book_to_library(book):
+
+
+
+

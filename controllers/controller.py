77c7a1ae900611834892f56667a33b6ccaa16396
Becky <bjentwistle@gmail.com>
2023-04-24 11:12:02 +0100

    additions to homework, inc another page to add book

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,14 +3,22 @@ from app import app
 from models.books import *
 from models.book import *
 
+@app.route("/home")
+def home():
+    return render_template('home.html', title = "Home Page")
+
 
 @app.route('/books') #default method is GET
 def index():
-    return render_template('index.html', title = "Home Page", books = books)
+    return render_template('index.html', title = "Books", books = books)
 
 @app.route("/books/<index>") #this will return the details of a chosen book
 def show_selected_book(index):
     return render_template("selected_book.html", book =books[int(index)]) #using the hyperlink added in the html/jinja file
+
+@app.route("/books/add")
+def books_add():
+    return render_template("add_book.html")
 
 @app.route('/books', methods = ['POST'])
 def add_book():
@@ -23,11 +31,11 @@ def add_book():
     unique_id = request.form['unique_id']
     new_book = Book(title, author, genre, loaned, location, synopsis, unique_id)
     add_new_book(new_book)
-    return render_template('index.html', title='Home', books=books) #add redirect here instead of render 
+    return redirect("/books")
 
 @app.route("/books/delete/<unique_id>", methods =['POST'])
 def remove_book(unique_id):
     for book in books:
         if unique_id == book.unique_id:
             books.remove(book)
-    return redirect("/books")
+    return redirect("/home")
